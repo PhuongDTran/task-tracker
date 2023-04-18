6d04ef6ea9b08f855213e5ff354c917d96d3d31f
Phuong Tran <fuongdtran@gmail.com>
2023-04-18 16:57:25 -0700

    task crud ops

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import * as React from 'react';
-import logo from './logo.svg';
+import { v4 as uuidv4 } from 'uuid';
+import { putData, getAllUserTasks, updateUserTask, deleteUserTask } from './dynamodbClient'
 import './App.css';
+import { addUserTask } from './dynamodbClient';
 
-import {putData} from './dynamodbClient'
-
-const USER_TASKS_TABLE_NAME = 'team57-user-tasks'
 
 function App() {
 
   React.useEffect(() => {
-    async function addData() {
-    const userData = {
-      username: "phuong",
-      taskId: "170"
-    }
 
-    await putData(USER_TASKS_TABLE_NAME , userData)
-  };
-  addData();
+    // add a task
+    const newTask = {
+      username: 'phuong',
+      taskId: uuidv4(),
+      title: "test",
+      status: "done",
+      description: 'test',
+      dueDate: 1681861699375 // time in millis
+    }
+    addUserTask(newTask, function (err, data) {
+      console.log(data)
+    });
+
+    // get all tasks belong to a username
+    getAllUserTasks('phuong', function (err, data) {
+      console.log(data)
+    });
+
+    //update a task
+    const task = {
+      username: 'phuong',
+      taskId: '170',
+      title: "test",
+      status: "done",
+      description: 'test',
+      dueDate: 56789834838 // mockup
+    }
+    updateUserTask(task, function(err, data){
+      if (err) {
+        console.log("err", err);
+      } else {
+        console.log("success", data)
+      }
+    })
+
+    // delete a task given username and taskId
+    deleteUserTask('Faisal', '170', function(err, data){
+      if (err) {
+        console.log("err", err);
+      } else {
+        console.log("success", data)
+      }
+    })
   }, [])
 
   return (
