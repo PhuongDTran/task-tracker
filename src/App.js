0b380026a4a66b996f129235378667215931c094
ConnieZi <conniezi1008@gmail.com>
2023-04-19 13:27:54 -0400

    Comment out demo code

--- src/App.js
+++ src/App.js
@@ -15,52 +15,52 @@ import LoginPage from "./LoginPage/LoginPage";
 
 function App() {
   
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  //const [isAuthenticated, setIsAuthenticated] = React.useState(false);
 
   React.useEffect(() => {
     // add a task
-    const newTask = {
-      username: "phuong",
-      taskId: uuidv4(),
-      title: "test",
-      status: "done",
-      description: "test",
-      dueDate: 1681861699375, // time in millis
-    };
-    addUserTask(newTask, function (err, data) {
-      console.log(data);
-    });
+    // const newTask = {
+    //   username: "phuong",
+    //   taskId: uuidv4(),
+    //   title: "test",
+    //   status: "done",
+    //   description: "test",
+    //   dueDate: 1681861699375, // time in millis
+    // };
+    // addUserTask(newTask, function (err, data) {
+    //   console.log(data);
+    // });
 
-    // get all tasks belong to a username
-    getAllUserTasks("phuong", function (err, data) {
-      console.log(data);
-    });
+    // // get all tasks belong to a username
+    // getAllUserTasks("phuong", function (err, data) {
+    //   console.log(data);
+    // });
 
     //update a task
-    const task = {
-      username: "phuong",
-      taskId: "170",
-      title: "test",
-      status: "done",
-      description: "test",
-      dueDate: 56789834838, // mockup
-    };
-    updateUserTask(task, function (err, data) {
-      if (err) {
-        console.log("err", err);
-      } else {
-        console.log("success", data);
-      }
-    });
+    // const task = {
+    //   username: "phuong",
+    //   taskId: "170",
+    //   title: "test",
+    //   status: "done",
+    //   description: "test",
+    //   dueDate: 56789834838, // mockup
+    // };
+    // updateUserTask(task, function (err, data) {
+    //   if (err) {
+    //     console.log("err", err);
+    //   } else {
+    //     console.log("success", data);
+    //   }
+    // });
 
     // delete a task given username and taskId
-    deleteUserTask("Faisal", "170", function (err, data) {
-      if (err) {
-        console.log("err", err);
-      } else {
-        console.log("success", data);
-      }
-    });
+    // deleteUserTask("Faisal", "170", function (err, data) {
+    //   if (err) {
+    //     console.log("err", err);
+    //   } else {
+    //     console.log("success", data);
+    //   }
+    // });
   }, []);
 
   return (
