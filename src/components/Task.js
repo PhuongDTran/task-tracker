a4b4f03e5805d4e4f460e9d9a622d999fa68892e
Avery Kyle Arenas <kyruken@gmail.com>
2023-04-20 10:29:33 -0700

    add removing task clientside

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,40 +1,47 @@
 import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
 import { deleteUserTask } from "../dynamodbClient";
-const handleClick = (username, taskId) => {
-    deleteUserTask(username, taskId, function (err, data) {
-        if (err) {
-            console.log("err", err);
-        } else {
-            console.log("success", data);
-        }
-    });
-}
+import { useState } from "react";
 
 function Task(props) {
+    const [isDeleted, setIsDeleted] = useState(false);
+
+    const handleClick = (username, taskId) => {
+        deleteUserTask(username, taskId, function (err, data) {
+            if (err) {
+                console.log("err", err);
+            } else {
+                console.log("success", data);
+            }
+        });
+        setIsDeleted(true);
+    }
+
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Title:
-                        {props.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Description:
-                        {props.description ? props.description : "No description"}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Status:
-                        {props.status ? props.status : "No status"}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Duedate:
-                        {props.due_date ? props.due_date : "No due date"}
-                    </Typography>
-                </CardContent>
-                <button onClick={() => handleClick(props.username, props.id)}>delete</button>
-            </CardActionArea>
-        </Card>
+        <>
+            {!isDeleted && <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Title:
+                            {props.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description:
+                            {props.description ? props.description : "No description"}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Status:
+                            {props.status ? props.status : "No status"}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Duedate:
+                            {props.due_date ? props.due_date : "No due date"}
+                        </Typography>
+                    </CardContent>
+                    <button onClick={() => handleClick(props.username, props.id)}>delete</button>
+                </CardActionArea>
+            </Card>}
+        </>
     )
 }
 
